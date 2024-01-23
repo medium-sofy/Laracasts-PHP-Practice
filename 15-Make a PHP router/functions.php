e100f677e7438e02de1cc403c581e17b0b1487c4
Mohamed <mediumsofy@gmail.com>
2024-01-23 02:13:46 +0200

    Add abort() function

--- 15-Make a PHP router/functions.php
+++ 15-Make a PHP router/functions.php
@@ -10,3 +10,9 @@ function dumpDie($value){
 function urlIs($url){
   return $_SERVER['REQUEST_URI'] == $url;
 }
+
+function abort($code = 404){
+  http_response_code($code);
+  require "views/{$code}.php"; 
+  die();
+}
--- 15-Make a PHP router/index.php
+++ 15-Make a PHP router/index.php
@@ -16,7 +16,5 @@ if(array_key_exists($uri,$routes)){
   require $routes[$uri];
 }
 else {
-  http_response_code(404);
-  require 'views/404.php'; 
-  die();
+ abort();
 }
