d51ce8295b096e113394c3dfdc817509431f10c3
Mohamed <mediumsofy@gmail.com>
2024-01-23 06:13:54 +0200

    Add initial code for '17-PDO First Steps'

---
+++ 17-PDO First Steps/router.php
@@ -0,0 +1,12 @@
+<?php
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes = [
+  '/' => 'controllers/index.php',
+
+  '/about' => 'controllers/about.php',
+
+  '/contact' => 'controllers/contact.php'
+];
+
+routeToControllers($uri,$routes);
