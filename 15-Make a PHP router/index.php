9421c0d33846d223119deec8619cab9c238bc4df
Mohamed <mediumsofy@gmail.com>
2024-01-23 06:10:35 +0200

    Refactor: add routing logic to router.php

--- 15-Make a PHP router/index.php
+++ 15-Make a PHP router/index.php
@@ -1,15 +1,4 @@
 <?php 
-
 require 'functions.php';
+require 'router.php';
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$routes = [
-  '/' => 'controllers/index.php',
-
-  '/about' => 'controllers/about.php',
-
-  '/contact' => 'controllers/contact.php'
-];
-
-routeToControllers($uri,$routes);
