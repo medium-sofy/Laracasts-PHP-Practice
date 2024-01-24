a352d4331fd2e45c4569cc2c63ae0dce5e138bc7
Mohamed <mediumsofy@gmail.com>
2024-01-24 08:46:57 +0200

    Extract fetchall() from query() method

--- 18-Extract a PHP Database Class/index.php
+++ 18-Extract a PHP Database Class/index.php
@@ -7,8 +7,11 @@ class Database {
   public $connection;
 
   public function __construct(){
+
     $dsn = 'mysql:host=localhost;port=3306;user=root;password=root;dbname=myapp;';
+
     $this->connection = new PDO($dsn);
+
   }
 
   public function query($query){   
@@ -17,13 +20,13 @@ class Database {
 
     $statement-> execute();
 
-    return $statement -> fetchall(PDO::FETCH_ASSOC);
+    return $statement;
 
   }
 }
 
 $db = new Database();
-$posts = $db->query('select * from posts');
+$posts = $db->query('select * from posts') -> fetchall(PDO::FETCH_ASSOC);
 
 foreach($posts as $post){
   echo '<li>'.$post['title'].'</li>';
