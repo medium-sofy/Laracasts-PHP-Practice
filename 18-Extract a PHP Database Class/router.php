99d3f22bdc938b96ac545c061295c9e3b8d4e51c
Mohamed <mediumsofy@gmail.com>
2024-01-24 07:55:58 +0200

    Add initial code for lesson 18

---
+++ 18-Extract a PHP Database Class/router.php
@@ -0,0 +1,11 @@
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes = [
+  '/' => 'controllers/index.php',
+
+  '/about' => 'controllers/about.php',
+
+  '/contact' => 'controllers/contact.php'
+];
+
+routeToControllers($uri,$routes);
