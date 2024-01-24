86259794c8092d37b2cbee6c2f2aa308d0922384
Mohamed <mediumsofy@gmail.com>
2024-01-24 09:24:37 +0200

    Fix typo in router.php

--- 17-PDO First Steps/index.php
+++ 17-PDO First Steps/index.php
@@ -1,4 +1,16 @@
 <?php 
 require 'functions.php';
-require 'router.php';
+//require 'router.php';
 
+$dsn = 'mysql:host=localhost;port=3306;user=root;password=root;dbname=myapp;';
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare('select * from posts');
+
+$statement-> execute();
+
+$posts = $statement -> fetchall(PDO::FETCH_ASSOC);
+
+foreach($posts as $post){
+  echo '<li>'.$post['title'].'</li>';
+}
