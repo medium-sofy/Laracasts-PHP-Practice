3751258eafd31568ce4c7be0644b6243701fbb35
Mohamed Sofy <mediumsofy@gmail.com>
2024-01-29 14:11:03 +0200

    Add initial code for lesson 23

---
+++ 23-Introduction to Authorization/index.php
@@ -0,0 +1,16 @@
+<?php 
+require 'functions.php';
+require 'Database.php';
+require 'router.php';
+require ('config.php');
+
+// $id =$_GET['id'];
+
+// $posts = $db->query("select * from posts where id = ?", [$id]) -> fetchall();
+
+//  dumpDie($posts);
+
+// Display posts and body content
+foreach($posts as $post){
+  echo '<li>'.$post['title'].': '. $post['body'].'</li>';
+}
