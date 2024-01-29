c71b161e59a9f67127b252dff3eff02ed2aceac3
Mohamed Sofy <mediumsofy@gmail.com>
2024-01-29 18:01:02 +0200

    Add initial code for lesson 25

---
+++ 25-Intro to Forms and Request Methods/index.php
@@ -0,0 +1,18 @@
+<?php 
+require 'Response.php';
+require 'Database.php';
+require 'functions.php';
+require 'router.php';
+require ('config.php');
+
+
+// $id =$_GET['id'];
+
+// $posts = $db->query("select * from posts where id = ?", [$id]) -> fetchall();
+
+//  dumpDie($posts);
+
+// Display posts and body content
+foreach($posts as $post){
+  echo '<li>'.$post['title'].': '. $post['body'].'</li>';
+}
