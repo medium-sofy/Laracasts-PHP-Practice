23087ba2d64bdfcccceed99d0f5fbb90d5c82f18
Mohamed Sofy <mediumsofy@gmail.com>
2024-01-29 18:28:45 +0200

    Add tailwind form component to note-create.view.php

--- 25-Intro to Forms and Request Methods/views/note-create.view.php
+++ 25-Intro to Forms and Request Methods/views/note-create.view.php
@@ -5,14 +5,46 @@
       
       <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
           
-          <form method="POST">
-            <textarea name="body"></textarea>
-            <p>
-              <button type="submit">
-                  Create
-              </button>
-            </p>
-          </form>
+          <!--
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+-->
+        <form method="POST">
+          <div class="space-y-12">
+            <div class="border-b border-gray-900/10 pb-12">      
+              <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                
+
+                <div class="col-span-full">
+                  <label for="body" class="block text-sm font-medium leading-6 text-gray-900">Create a New Note</label>
+                  <div class="mt-2">
+                    <textarea id="body" name="body" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                  </div>
+                </div>      
+              </div>
+            </div>
+
+            
+            </div>
+
+            
+          </div>
+
+          <div class="flex items-center justify-center gap-x-6">
+            <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create</button>
+          </div>
+        </form>
+
       </div>
 
     </main>
