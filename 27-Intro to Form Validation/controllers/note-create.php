5d886ed4f5f175604e5f4b71c43b97204e05bb56
Mohamed Sofy <mediumsofy@gmail.com>
2024-01-30 08:46:07 +0200

    Add logic to prevent empty form submission

--- 27-Intro to Form Validation/controllers/note-create.php
+++ 27-Intro to Form Validation/controllers/note-create.php
@@ -4,11 +4,22 @@ $db = new Database($config['Database']);
 $heading = 'Create a New Note';
 
 if($_SERVER['REQUEST_METHOD']==='POST'){
-      $db -> query('INSERT INTO notes(body, user_id) VALUES(:body,:user_id)',
+     
+  $errors = [];
+
+  if (strlen($_POST['body']) === 0) {
+    $errors['body'] = 'Error! A body is required';
+  }
+  
+  if(empty($errors))
+  {
+  $db -> query('INSERT INTO notes(body, user_id) VALUES(:body,:user_id)',
       [
         'body' => $_POST['body'],
         'user_id' => 9
       ]);
+  }
+  
   }
 
 require 'views/note-create.view.php';
--- 27-Intro to Form Validation/views/note-create.view.php
+++ 27-Intro to Form Validation/views/note-create.view.php
@@ -29,6 +29,9 @@
                   <label for="body" class="block text-sm font-medium leading-6 text-gray-900">Create a New Note</label>
                   <div class="mt-2">
                     <textarea id="body" name="body" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                    <?php if(isset($errors['body'])):?>
+                        <p class="text-red-500 text-xs mt-2"> <?= $errors['body']?> </p>
+                    <?php endif ?>
                   </div>
                 </div>      
               </div>
