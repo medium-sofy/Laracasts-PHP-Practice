17a6e35e72b2da182b2c83a53f2c7018d3d524f5
Mohamed Sofy <mediumsofy@gmail.com>
2024-01-30 09:17:36 +0200

    Add logic to keep form data in the case of an error

--- 27-Intro to Form Validation/views/note-create.view.php
+++ 27-Intro to Form Validation/views/note-create.view.php
@@ -19,7 +19,7 @@
   }
   ```
 -->
-        <form method="POST">
+        <form action="" method="POST">
           <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">      
               <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
@@ -28,7 +28,9 @@
                 <div class="col-span-full">
                   <label for="body" class="block text-sm font-medium leading-6 text-gray-900">Create a New Note</label>
                   <div class="mt-2">
-                    <textarea id="body" name="body" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                    <textarea id="body" name="body" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                     <?= $_POST['body'] ?? '' ?>
+                  </textarea>
                     <?php if(isset($errors['body'])):?>
                         <p class="text-red-500 text-xs mt-2"> <?= $errors['body']?> </p>
                     <?php endif ?>
