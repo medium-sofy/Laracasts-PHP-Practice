a54f508dd45fb737f1e1a6ae227085a9c7ea6995
Mohamed Sofy <mediumsofy@gmail.com>
2024-01-30 11:12:23 +0200

    Add a Validator class to check form input consistency

--- 28-Extract a Simple Validator Class/controllers/note-create.php
+++ 28-Extract a Simple Validator Class/controllers/note-create.php
@@ -1,20 +1,19 @@
 <?php
+require 'Validator.php';
 $config = require('config.php');
 $db = new Database($config['Database']);
 $heading = 'Create a New Note';
 
+
 if($_SERVER['REQUEST_METHOD']==='POST'){
      
   $errors = [];
-
-  if (strlen($_POST['body']) === 0) {
-    $errors['body'] = 'Error! A body is required';
-  }
-
-  if(strlen($_POST['body']) > 1000){
-    $errors['body'] = 'The body can\'t be more than 1,000 characters!';
-  }
   
+  if (! Validator::string($_POST['body'], 1, 1000)) {
+
+    $errors['body'] = 'A body of no more than 1,000 characters is required';
+  }
+
   if(empty($errors))
   {
   $db -> query('INSERT INTO notes(body, user_id) VALUES(:body,:user_id)',
