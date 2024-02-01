20cd5534164c03bc3148d224280aa5635c2e6226
Mohamed Sofy <mediumsofy@gmail.com>
2024-02-01 07:37:52 +0200

    30-Add Project files and folders

---
+++ controllers/notes/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = require base_path('config.php');
+$db = new Database($config['database']);
+
+$notes = $db->query('select * from notes where user_id = 9')->get();
+
+view("notes/index.view.php", [
+    'heading' => 'My Notes',
+    'notes' => $notes
+]);
