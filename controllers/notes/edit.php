2406381338ef5022ab6a14e2b71b37598869a89c
Mohamed Sofy <mediumsofy@gmail.com>
2024-02-04 15:50:49 +0200

    36-Add note edit controller

---
+++ controllers/notes/edit.php
@@ -0,0 +1,19 @@
+<?php
+use Core\Database;
+use Core\App;
+
+$db = App::resolve(Database::class);
+
+$currentUserId = 9;  
+
+$note = $db->query('select * from notes where id = :id', [
+    'id' => $_GET['id']
+])->findOrFail();
+
+authorize($note['user_id'] === $currentUserId);
+
+view("notes/edit.view.php", [
+    'heading' => 'Edit Note',
+    'errors' => [],
+    'note' => $note
+]);
