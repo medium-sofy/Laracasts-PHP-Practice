da985622a000739feff833291f5b2496d01e5cdb
Mohamed Sofy <mediumsofy@gmail.com>
2024-02-04 10:31:50 +0200

    34-Create destroy.php to handle note delete request

---
+++ controllers/notes/destroy.php
@@ -0,0 +1,20 @@
+<?php
+
+use Core\Database;
+
+$config = require base_path('config.php');
+$db = new Database($config['database']);
+
+$currentUserId = 9;  
+
+
+  $note = $db->query('select * from notes where id = :id', [
+    'id' => $_POST['id']])->findOrFail();
+
+    authorize($note['user_id'] === $currentUserId);
+
+  $db->query('delete from notes where id = :id',
+            ['id' => $_POST['id']]);
+
+  header('Location: /notes');
+  exit();    
