29f9ec986591b070e4026e1502be6f3fcdbc80ea
Mohamed Sofy <mediumsofy@gmail.com>
2024-02-05 12:10:27 +0200

    38-Add static method email() to Validator class

--- Lessons-Archive: 6-29/28-Extract a Simple Validator Class/Validator.php
+++ Lessons-Archive: 6-29/28-Extract a Simple Validator Class/Validator.php
@@ -10,4 +10,9 @@ class Validator{
     return strlen($value) >= $min && strlen($value) <= $max;
     
   }
+
+  public static function email(string $value): bool
+  {
+      return filter_var($value, FILTER_VALIDATE_EMAIL);
+  }
 } 
