a54b4a13299788ef52e81e97e42cd1de292650c6
Mohamed Sofy <mediumsofy@gmail.com>
2024-02-07 13:21:31 +0200

    40-Add password hashing when registering a new user

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -42,7 +42,7 @@ else
     $db->query('INSERT INTO users(email, password) VALUES(:email, :password)',
     [
       'email'=> $email,
-      'password' => $password
+      'password' => password_hash($password, PASSWORD_BCRYPT)
     ]);
     // work that the user has logged in
     $_SESSION['user'] = [
