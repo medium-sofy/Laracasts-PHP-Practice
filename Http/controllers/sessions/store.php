8571e337b9c671804d0b4f6a04ae83f726887d13
Mohamed Sofy <mediumsofy@gmail.com>
2024-02-08 22:37:02 +0200

    43-Rewrite Login form handling logic

--- Http/controllers/sessions/store.php
+++ Http/controllers/sessions/store.php
@@ -1,10 +1,7 @@
 <?php
 
-use Core\App;
-use Core\Database;
+use Core\Authenticator;
 use Http\Forms\LoginForm;
-
-$db = App::resolve(Database::class);
 
 $email = $_POST['email'];
 $password = $_POST['password'];
@@ -12,37 +9,19 @@ $password = $_POST['password'];
 // validate form input
 $form = new LoginForm;
 
-if(! $form->validate($email,$password)){
-  return view('sessions/create.view.php',[
-    'errors' => $form->errors(),
-    'email' => $email
-  ]);   
-}
+if($form->validate($email,$password)){
+  
+  if((new Authenticator)->attempt($email,$password))
+  {
+      redirect('/');
 
-// match the credentials
+  } else {
 
-$user = $db->query('SELECT * FROM users WHERE email = :email',
-      [
-        'email' => $email
-      ])->find();
-
-if($user){
-  // if the user exist in the database, and the password provided matches the one associated with that email,
-  // only on that case we log the user in
-  if(password_verify($password, $user['password'])){
-
-    // associate the user email to the session which identifies him as logged in
-    login($user);
-
-    header('Location: /');
+      $form-> error('email',"No match was found for {$email} and the provided password");
   }
 }
-// Display an error if the user doesn't exist, or he exists but the password is incorrect
 
 return view('sessions/create.view.php',[
-  'errors' => 
-      [
-        'email' => "No match was found for {$email} and the provided password"
-      ],
+  'errors' => $form->errors(),
   'email' => $email
 ]);
