eb613ab28f9c41769ae173eb5899b49a0bd69e85
Mohamed Sofy <mediumsofy@gmail.com>
2024-02-08 22:35:40 +0200

    43-Add error() method to LoginForm class

--- Http/Forms/LoginForm.php
+++ Http/Forms/LoginForm.php
@@ -5,23 +5,28 @@ use Core\Validator;
 
 class LoginForm
 {
-    protected $errors = [];
+  protected $errors = [];
 
-    public function validate($email, $password)
-    {
-      if(!Validator::email($email)){
-        $this->errors['email'] = 'Please provide a valid email address';
-      }
-      
-      if(!Validator::string($password)){
-        $this->errors['password'] = 'Please provide a valid password';
-      }
-
-      return empty($this->errors);
+  public function validate($email, $password)
+  {
+    if (!Validator::email($email)) {
+      $this->errors['email'] = 'Please provide a valid email address';
     }
 
-    public function errors()
-    {
-      return $this->errors;
+    if (!Validator::string($password)) {
+      $this->errors['password'] = 'Please provide a valid password';
     }
+
+    return empty($this->errors);
+  }
+
+  public function errors()
+  {
+    return $this->errors;
+  }
+
+  public function error($field, $message)
+  {
+    $this->errors[$field] = $message;
+  }
 }
